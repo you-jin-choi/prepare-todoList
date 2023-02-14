1dbf1d614133092cf6d591f30c56823e9d4677e2
youjin <uzn.biz@gmail.com>
2023-02-14 16:27:29 +0900

    first

---
+++ src/main/java/com/assignment/preparetodoList/PrepareTodoListApplication.java
@@ -0,0 +1,13 @@
+package com.assignment.preparetodoList;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PrepareTodoListApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PrepareTodoListApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/assignment/preparetodoList/PrepareTodoListApplicationTests.java
@@ -0,0 +1,13 @@
+package com.assignment.preparetodoList;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PrepareTodoListApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

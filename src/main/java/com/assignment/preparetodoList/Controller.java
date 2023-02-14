bf88c2f3c9786d4fdee8780cf4ff3cbee25a3c10
youjin <uzn.biz@gmail.com>
2023-02-14 16:46:55 +0900

    first feature

---
+++ src/main/java/com/assignment/preparetodoList/Controller.java
@@ -0,0 +1,13 @@
+package com.assignment.preparetodoList;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class Controller {
+
+    @GetMapping("/")
+    public String hello() {
+        return "To-do Application!";
+    }
+}
